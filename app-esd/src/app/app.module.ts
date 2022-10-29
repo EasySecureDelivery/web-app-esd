5c3555d2a689ed1bdbbfc4dbceeeadb237e2968c
dego137 <diegohmayang@hotmail.com>
2022-10-29 03:33:05 -0500

    Se agrego routes y modulos

--- app-esd/src/app/app.module.ts
+++ app-esd/src/app/app.module.ts
@@ -4,45 +4,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+
+//componentes
 import { LoginComponent } from './vista/login/login.component';
-import { TrackingComponent } from './vista/tracking/tracking.component';
-import { TransportHeaderComponent } from './vista/transport/transport-header/transport-header.component';
-import { MyaccountComponent } from './vista/transport/myaccount/myaccount.component';
-import { ProfileComponent } from './vista/transport/profile/profile.component';
-import { MyshipmentstransportComponent } from './vista/transport/myshipmentstransport/myshipmentstransport.component';
-import { RegistershipmentsComponent } from './vista/transport/registershipments/registershipments.component';
-import { EmpresarioHeaderComponent } from './vista/empresario/empresario-header/empresario-header.component';
-import { IpickupComponent } from './vista/empresario/ipickup/ipickup.component';
-import { AddresseComponent } from './vista/empresario/addresse/addresse.component';
-import { DatapersonComponent } from './vista/empresario/dataperson/dataperson.component';
-import { SelectcompanyComponent } from './vista/empresario/selectcompany/selectcompany.component';
-import { TicketshipmentComponent } from './vista/empresario/ticketshipment/ticketshipment.component';
+import { SharedModule } from './shared/shared.module';
 import { ShippingstatusComponent } from './vista/empresario/shippingstatus/shippingstatus.component';
 import { MyshipmentsComponent } from './vista/empresario/myshipments/myshipments.component';
+import { DatapersonComponent } from './vista/empresario/dataperson/dataperson.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    TrackingComponent,
-    TransportHeaderComponent,
-    MyaccountComponent,
-    ProfileComponent,
-    MyshipmentstransportComponent,
-    RegistershipmentsComponent,
-    EmpresarioHeaderComponent,
-    IpickupComponent,
-    AddresseComponent,
-    DatapersonComponent,
-    SelectcompanyComponent,
-    TicketshipmentComponent,
     ShippingstatusComponent,
-    MyshipmentsComponent
+    MyshipmentsComponent,
+    DatapersonComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ app-esd/src/app/shared/shared.module.ts
@@ -0,0 +1,59 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+//modulos 
+import { ReactiveFormsModule } from '@angular/forms';
+
+
+
+//angular material
+import { MatSliderModule } from '@angular/material/slider';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import { HttpClientModule } from '@angular/common/http';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import {MatRadioModule} from '@angular/material/radio';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatSliderModule,
+    MatInputModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    MatSnackBarModule,
+    MatProgressSpinnerModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatFormFieldModule,
+    HttpClientModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatRadioModule,
+  ],
+  exports:[
+
+    MatSliderModule,
+    MatInputModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    MatSnackBarModule,
+    MatProgressSpinnerModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatFormFieldModule,
+    HttpClientModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatRadioModule,
+  ]
+})
+export class SharedModule { }
