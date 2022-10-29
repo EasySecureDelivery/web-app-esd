e5323697809ca9d6c3f6c44a39157843161acc21
dego137 <diegohmayang@hotmail.com>
2022-10-29 00:51:32 -0500

    Se instalo json server

--- app-esd/src/app/vista/empresario/myshipments/myshipments.component.ts
+++ app-esd/src/app/vista/empresario/myshipments/myshipments.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatTableDataSource } from '@angular/material/table';
+import { Shipment } from '../../model/shipment';
+import { ShipmentService } from '../../services/shipment.service';
 
 @Component({
   selector: 'app-myshipments',
@@ -6,10 +11,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./myshipments.component.css']
 })
 export class MyshipmentsComponent implements OnInit {
-
-  constructor() { }
-
+  displayedColumns: String[] = [
+    'id',
+    'content',
+    'date',
+    'status',
+    'contact',
+  ];
+  dataSource= new MatTableDataSource<Shipment>();
+  
+  clickedRows = new Set<Shipment>();
+  Shipment!: Shipment[];
+  @ViewChild(MatPaginator, { static: true }) paginator!: MatPaginator;
+  constructor(private ShipmentService: ShipmentService, private snackBar: MatSnackBar) { }
   ngOnInit(): void {
+    this.getShipment();
+  }
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
+  getShipment() {
+    this.ShipmentService.getShipment().subscribe((data: Shipment[]) => {
+      this.dataSource = new MatTableDataSource(data);
+      this.dataSource.paginator = this.paginator;
+    });
+  }
+
+  deleteShipment(id: number) {
+    this.ShipmentService.deleteShipment(id).subscribe(() => {
+      this.dataSource.data = this.dataSource.data.filter((e: Shipment) => {
+        return e.id !== id ? e : false;
+      });
+    });
+  }
 }
